5510a43d974b1bc3e0c337883bada7f902fcf296
Jungjae <100228417+jungjae01eng@users.noreply.github.com>
2022-10-14 22:07:29 -0400

    Add files via upload

--- PA_5_1_main.py
+++ PA_5_1_main.py
@@ -8,13 +8,14 @@ import math
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import scipy.stats as stat
+# import scipy.stats as stat
 from scipy.stats import norm
 from scipy.stats import expon
 
 
 benign = np.genfromtxt("PA_5_2_Benigndata.csv", delimiter = ",")
 malignant = np.genfromtxt("PA_5_3_Malignantdata.csv", delimiter = ",")
+uniteddelays = np.genfromtxt("PA_5_4_Uniteddelays.csv", delimiter = ",")
 
 
 # ===== Part (a) =====
@@ -40,33 +41,77 @@ print(malignant_var)
 
 
 # ===== Part (c) =====
-# Plot normalized histogram <benign>
-plt.hist(benign, bins = "auto", density = True)
+# === <BENIGN 1> ===
+# Plot normalized histogram
+plt.figure(1)
+plt.hist(benign[:, 0], bins = "auto", density = True)
 xmin, xmax = plt.xlim()
 xpoints = np.linspace(xmin, xmax, 200)
 
 # PDF for Gaussian with mean and standard deviation
-benign_pdf = norm.pdf(xpoints, benign_mean, math.sqrt(benign_var))
+benign_pdf_1 = norm.pdf(xpoints, benign_mean[0], math.sqrt(benign_var[0]))
 
 # Plot PDF
-plt.plot(xpoints, benign_pdf, "r", linewidth = 2)
+plt.plot(xpoints, benign_pdf_1, "r", linewidth = 2)
 
 
-# Plot normalized histogram <malignant>
-plt.hist(malignant, bins = "auto", density = True)
+# === <BENIGN 2> ===
+# Plot normalized histogram
+plt.figure(2)
+plt.hist(benign[:, 1], bins = "auto", density = True)
 xmin, xmax = plt.xlim()
+xpoints = np.linspace(xmin, xmax, 200)
 
 # PDF for Gaussian with mean and standard deviation
-malignant_pdf = norm.pdf(xpoints, malignant_mean, math.sqrt(malignant_var))
+benign_pdf_2 = norm.pdf(xpoints, benign_mean[1], math.sqrt(benign_var[1]))
 
 # Plot PDF
-plt.plot(xpoints, malignant_pdf, "r", linewidth = 2)
+plt.plot(xpoints, benign_pdf_2, "r", linewidth = 2)
+
+
+# === <MALIGNANT 1> ===
+# Plot normalized histogram
+plt.figure(3)
+plt.hist(malignant[:, 0], bins = "auto", density = True)
+xmin, xmax = plt.xlim()
+xpoints = np.linspace(xmin, xmax, 200)
+
+# PDF for Gaussian with mean and standard deviation
+malignant_pdf_1 = norm.pdf(xpoints, malignant_mean[0], math.sqrt(malignant_var[0]))
+
+# Plot PDF
+plt3 = plt.plot(xpoints, malignant_pdf_1, "r", linewidth = 2)
+
+
+# === <MALIGNANT 2> ===
+# Plot normalized histogram
+plt.figure(4)
+plt.hist(malignant[:, 1], bins = "auto", density = True)
+xmin, xmax = plt.xlim()
+xpoints = np.linspace(xmin, xmax, 200)
+
+# PDF for Gaussian with mean and standard deviation
+malignant_pdf_2 = norm.pdf(xpoints, malignant_mean[1], math.sqrt(malignant_var[1]))
+
+# Plot PDF
+plt.plot(xpoints, malignant_pdf_2, "r", linewidth = 2)
 
 
 # ===== Part (d) =====
 # Calculate mean
-benign_p = expon.pdf(xpoints, scale = benign_mean)
-malignant_p = expon.pdf(xpoints, scale = malignant_mean)
+plt.figure (5)
+uniteddelays_mean = np.mean(uniteddelays, axis = 0)
+
+# Plot the histogram
+plt.hist(uniteddelays, bins = "auto", density = True)
+xmin, xmax = plt.xlim()
+xpoints = np.linspace(xmin, xmax, 200)
+
+# Evaluation of the exponential PDF
+uniteddelays_p = expon.pdf(xpoints, scale = uniteddelays_mean)
+plt.plot(xpoints, uniteddelays_p, "r", linewidth = 2)
+plt.show()
 
 
 # ===== Part (e) =====
+print(uniteddelays_mean)
